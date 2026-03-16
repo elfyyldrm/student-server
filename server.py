991d9e4aad8447fc9d1f01c6072757072bb557db
Elif Yıldırım <41282056+elfyyldrm@users.noreply.github.com>
2026-03-16 13:36:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Submission(BaseModel):
+    student_id: str
+    answer: str
 
 @app.get("/")
 def home():
     return {"message": "Server is running"}
+
+@app.post("/submit")
+def submit(data: Submission):
+    print("New submission:", data)
+    return {
+        "status": "received",
+        "student_id": data.student_id
+    }
